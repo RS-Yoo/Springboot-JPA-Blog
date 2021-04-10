5f375206a5a628ffebafd6a86413f1f0e024df85
Mary <rsy9807@gmail.com>
2021-04-10 15:29:20 -0700

    Join with Ajax

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -11,11 +11,29 @@
 			username: $("#username").val(),
 			password: $("#password").val(),
 			email: $("#email").val(),
-		}
+		};
 		
 		//console.log(data);
 		
-		$.ajax().done().fail(); // ajax 통신을 이용해서 3개의 데이터를 json으로 변경, insert 요청 
+		// ajax 호출시 default 는 비동기 호출
+		// ajax 통신을 이용해서 3개의 데이터를 json으로 변경, insert 요청 
+		// ajax가 통신 성공 후 서버가 json 리턴하면 자동으로 자바 오브젝트로 변환
+		$.ajax( {
+			// 회원가입 수행 요청
+			type: "POST",
+			url: "/blog/api/user",
+			data: JSON.stringify(data), // http body data
+			contentType: "application/json; charset=utf-8", // body data type(MIME)
+			dataType: "json" // 요청에 대한 응답이 왔을 때 기본적으로 모든 것이 문자열 // 생긴 게 json이라면 js오브젝트로 변경
+		}).done(function(resp) {
+			// 성공했을 때
+			alert("Registration complete.");
+			alert(resp);
+			location.href = "/blog";
+		}).fail(function(error) {
+			//실패했을 때
+			alert(JSON.stringify(error));
+		}); 
 	}
 }
 
